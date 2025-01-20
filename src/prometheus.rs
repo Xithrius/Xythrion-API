3b44f358b8a2fc36f5c7e93b06b6d41d8052c703
Xithrius <xithrius@fastmail.com>
2025-01-20 02:41:53 -0800

    App name bound to xythrion-api so variables can be used in grafana

--- src/prometheus.rs
+++ src/prometheus.rs
@@ -1,8 +1,14 @@
+use std::collections::HashMap;
+
 use actix_web_prom::{PrometheusMetrics, PrometheusMetricsBuilder};
 
 pub fn prometheus_middleware() -> PrometheusMetrics {
-    PrometheusMetricsBuilder::new("xythrion_api")
+    let mut labels = HashMap::new();
+    labels.insert("app_name".to_string(), "xythrion-api".to_string());
+
+    PrometheusMetricsBuilder::new("api")
         .endpoint("/metrics")
+        .const_labels(labels)
         .build()
         .unwrap()
 }
